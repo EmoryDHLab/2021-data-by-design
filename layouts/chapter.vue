b2dcb044312851c052970709733ff3bdc65d3544
Dan Jutan <danjutan@gmail.com>
2021-06-16 12:26:13 -0400

    chapter layout

---
+++ layouts/chapter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This "header" goes on every chapter. It knows about the current route: {{$nuxt.$route.name}}
+    <Nuxt></Nuxt>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/chapters/peabody.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    peabody content
+  </div>
+</template>
+
+<script>
+export default {
+  name: "peabody",
+  layout: "chapter"
+}
+</script>
+
+<style scoped>
+
+</style>
