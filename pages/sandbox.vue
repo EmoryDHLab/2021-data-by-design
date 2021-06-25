75ae42b97f3a7b93a471dadec883d21bf0644a67
Dan Jutan <danjutan@gmail.com>
2021-06-24 23:28:44 -0400

    bring in (broken) penguin demo

---
+++ pages/sandbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <LeaderFollow></LeaderFollow>
+</template>
+
+<script>
+import LeaderFollow from "~/components/page-layout/LeaderFollow";
+export default {
+  components: {LeaderFollow}
+}
+</script>
+
+<style scoped>
+
+</style>
