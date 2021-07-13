d886e50f2f7980462afa745ac724882ba62a79f1
Dan Jutan <danjutan@gmail.com>
2021-07-13 15:57:06 -0400

    frontpage top grid setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,55 @@
 <template>
   <div class="container">
-    <NuxtLink to="/chapters/peabody">Peabody</NuxtLink>
+<!--    <NuxtLink to="/chapters/peabody">Peabody</NuxtLink>-->
+    <div class="big-title">
+      <span class="line-one">DATA BY</span>
+      <span class="line-two">DESIGN</span>
+    </div>
+    <div class="cover-img-container">
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
-
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  background-color: black;
+  display: grid;
+  grid-template-columns: repeat(14, 1fr);
+  grid-template-rows: repeat(10, 81px);
+  width: 100%;
+}
+.big-title {
+  color: white;
+  font-weight: bold;
+  font-size: 224px;
+  grid-column: 1 / -1;
+  grid-row: 1 / 4;
+  /*line-height: 80%;*/
+  margin-left: 40px;
+  z-index: 10;
+  line-height: 80%;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.big-title .line-one {
+  font-family: "VTC William";
+}
+.big-title .line-two {
+  font-family: "VTC William Wide";
+  /*grid-column: 2 / -1;*/
+  /*grid-row: 3 / -1;*/
+  margin-top: 10px;
+  /*z-index: 10;*/
+  margin-left: 112px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.cover-img-container {
+  background-color: #3B6FE0;
+  grid-column: 10 / -1;
+  grid-row: 1 / -1;
 }
 
-.links {
-  padding-top: 15px;
-}
 </style>
