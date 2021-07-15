5206f387167e7a36313e47cb1d74ad62f0a6999a
Dan Jutan <danjutan@gmail.com>
2021-07-15 13:37:09 -0400

    commit before branch

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,35 @@
       <span class="line-one">DATA BY</span>
       <span class="line-two">DESIGN</span>
     </div>
-    <div class="cover-img-container">
-
+    <div class="subtitle">
+      An Interactive History of Data Visualization<br>
+      1786-1900
     </div>
+    <div class="question-one">
+        What is the story we tell about the emergence of modern data visualization?
+      </div>
+    <div class="question-two">
+      How might we tell that story differently?
+    </div>
+    <div class="cover-img-container"></div>
+    <Captioned class="cover-img">
+      <img src="../assets/images/ch5-08b-amalg.jpg"></img>
+      <template v-slot:caption>
+        <span :style="{color: 'white'}">
+          The Amalgamation of White and Black elements of the population in the United States by W.E.B Du Bois.
+        Atlanta University. Library of Congress.
+        </span>
+      </template>
+    </Captioned>
   </div>
 </template>
 
 <script>
-export default {}
+import LocalImage from "@/components/global/docs-inclusions/LocalImage";
+import Captioned from "@/components/global/Captioned";
+export default {
+  components: {Captioned}
+}
 </script>
 <style>
 .container {
@@ -21,12 +42,13 @@ export default {}
   grid-template-columns: repeat(14, 1fr);
   grid-template-rows: repeat(10, 81px);
   width: 100%;
+  color: white;
+  font-family: "VTC William";
 }
 .big-title {
-  color: white;
   font-weight: bold;
   font-size: 224px;
-  grid-column: 1 / -1;
+  grid-column: 1 / 11;
   grid-row: 1 / 4;
   /*line-height: 80%;*/
   margin-left: 40px;
@@ -35,7 +57,6 @@ export default {}
 }
 
 .big-title .line-one {
-  font-family: "VTC William";
 }
 .big-title .line-two {
   font-family: "VTC William Wide";
@@ -46,10 +67,38 @@ export default {}
   margin-left: 112px;
 }
 
+.subtitle {
+  font-size: 36px;
+  grid-column: 6 / 10;
+  grid-row: 6
+}
+
+.question-one {
+  font-size: 24px;
+  grid-column: 2 / 5;
+  grid-row: 8 / span 2;
+}
+
+.question-two {
+  font-size: 24px;
+  grid-column: 6 / 9;
+  grid-row: 8 / span 2;
+}
+
 .cover-img-container {
   background-color: #3B6FE0;
   grid-column: 10 / -1;
-  grid-row: 1 / -1;
+  grid-row: 1 / 10;
+  margin-top: 40px;
+}
+
+.cover-img {
+  grid-column: 11 / -2;
+  grid-row: 2 / 8;
+}
+
+.cover-img img {
+  max-width: 100%;
 }
 
 </style>
