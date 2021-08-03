06d17af06ac1fd3e05c6e0697cde5ef351a90270
Dan Jutan <danjutan@gmail.com>
2021-08-03 16:20:46 -0400

    full screen, non-collect scrollytell

--- components/global/docs-inclusions/Scrollytell.vue
+++ components/global/docs-inclusions/Scrollytell.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="root" ref="root" :style="localVars">
-    <div class="scrolly">
-      <div v-for="i in groups" ref="groups">
+  <div class="col-span-full" :style="totalHeightStyle" ref="root">
+
+
+    <!--We want this background to match the height of all the scrolly groups.
+    But we can't actually make it the background of that container
+    (or have it be part of the child containers - I've tried a lot of stuff) because it needs to be full
+    width, which these non-absolute containers can't be thanks to our grid layout.
+    -->
+    <div ref="background" class="bg-theme absolute z-0 left-0 w-screen" :style="totalHeightStyle">
+
+    </div>
+
+    <div class="float-right mr-4 pt-8 sticky top-0 z-10" ref="sticky">
+      <slot name="sticky"></slot>
+    </div>
+
+    <div class="w-full ml-3 mb-96 z-10 relative" ref="scrolly">
+      <div class="w-1/2 h-screen"  ref="buffer"></div>
+      <div v-for="(i, zeroIndexed) in groups" class="w-1/2 h-screen text-theme"
+           :class="{'sticky bottom-0': collect && zeroIndexed == 0}"
+           ref="groups">
         <slot :name="'group:' + i"></slot>
       </div>
     </div>
-    <GridLayout class="grid" ref="grid">
-      <div class="sticky">
-        <slot name="sticky"></slot>
-      </div>
-    </GridLayout>
+
+
   </div>
 </template>
 
@@ -36,7 +51,11 @@ export const docsDefinition = {
 export default {
   components: {GridLayout},
   props: {
-    groups: Number
+    groups: Number,
+    collect: {
+      type: Boolean,
+      default: false
+    }
   },
   provide () {
     return {
@@ -44,72 +63,69 @@ export default {
     }
   },
   data: () => ({
+    totalHeight: -1,
+    scrollytellActive: false,
     scrollData: {
       progress: -1,
       current: undefined
     }
   }),
   computed: {
-    localVars() {
-      const gap = "600px";
-      return {
-        "--numGroups": this.groups,
-        "--gap": gap
+    totalHeightStyle () {
+      if (this.totalHeight > 0) {
+        return {
+          height: this.totalHeight + "px"
+        }
       }
     }
+    // localVars() {
+    //   const gap = "600px";
+    //   return {
+    //     "--numGroups": this.groups,
+    //     "--gap": gap
+    //   }
+    // }
   },
   mounted() {
+    this.totalHeight = this.$refs.scrolly.clientHeight;
     setTimeout(() => {
+
+      // ScrollTrigger.create({
+      //   trigger: this.$refs.grid.$el,
+      //   start: "bottom bottom",
+      //   onToggle: ({progress, direction, isActive}) => console.log(progress, direction, isActive),
+      // })
+
+
       const lastGroup = this.$refs.groups[this.groups - 1];
+      console.log(this.$refs);
       ScrollTrigger.create({
         anticipatePin: 1,
-        trigger: this.$refs.grid.$el,
+        trigger: this.$refs.sticky,
         endTrigger: lastGroup,
-        start: "top 100px",
-        // onToggle: ({progress, direction, isActive}) => console.log(progress, direction, isActive),
-        pin: this.$refs.grid.$el,
-        end: "top 100px"
+        start: "top top",
+        onToggle: ({progress, direction, isActive}) => {
+          this.scrollytellActive = isActive;
+          // console.log(progress, direction, isActive),
+        },
+        // pin: this.$refs.sticky,
+        end: "top 100px",
       })
 
-      for (let i = 0; i < this.groups; i++) {
+      const groups = [this.$refs.buffer, ...this.$refs.groups];
+
+      groups.forEach((el, i) => {
         ScrollTrigger.create({
-          trigger: this.$refs.groups[i],
-          start: "top 750px",
-          end: "bottom 100px",
+          trigger: el,
+          start: "bottom bottom",
+          end: "bottom top",
           onUpdate: ({progress}) => this.scrollData.progress = progress,
           onEnter: (instance) => this.scrollData.current = i,
           onLeaveBack: (instance) => this.scrollData.current = i - 1,
         })
-      }
+      })
     }, 2100)
   }
 }
 </script>
 
-<style scoped>
-.root {
-  grid-column: 1 / -1;
-}
-
-.grid {
-  background-color: var(--primaryColor);
-  margin: var(--vertical-gap-large) 0;
-  height: 650px;
-}
-
-.scrolly {
-  z-index: 10;
-  position: absolute;
-  margin-left: 100px;
-  margin-top: 100px;
-}
-
-.scrolly > div {
-  margin-top: 650px;
-}
-
-.sticky {
-  grid-column: var(--halfway-line) / var(--num-col)
-}
-
-</style>
