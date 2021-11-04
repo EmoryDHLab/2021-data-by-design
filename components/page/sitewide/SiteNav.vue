fe127a010195a482156587a339ba9bd94fe66b83
Anna Mola <annamola12345@gmail.com>
2021-11-04 19:35:33 +0000

    start nav refactor

--- components/page/sitewide/SiteNav.vue
+++ components/page/sitewide/SiteNav.vue
@@ -1,53 +1,62 @@
 <template>
-  <div class="site-nav-root">
-    <div class="flex">
-      <div class="site-title">
-        Data by Design
-      </div>
-      <div class="nav-title">
-        Home
-      </div>
-      <div class="nav-title">
-        Chapters
-      </div>
-      <div class="nav-title">
-        About the Site
-      </div>
-      <div class="nav-title">
-        Credits
+  <div class="w-full bg-black flex justify-evenly">
+    <div class="font-william text-white text-2xl">
+      Data by Design
+    </div>
+    <div class="flex justify-between w-2/5">
+      <div
+        class="font-william text-white text-xl"
+        v-for="page in pages"
+        @mouseover="onHover(page)"
+        @mouseout="hoverPage = ''"
+      >
+        <NuxtLink :to="page.to">{{ page.name }}</NuxtLink>
+        <div v-if="page.children">
+          <div v-for="child in page.children">
+            <NuxtLink :to="child.to">{{ child.name }}</NuxtLink>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import chaptersConfig from "@/chapters-config";
+
+const chaptersChildren = Object.values(chaptersConfig).map(
+  ({ navName, id }) => ({
+    to: `/chapters/${id}/`,
+    name: navName
+  })
+);
+
 export default {
-}
+  data() {
+    return {
+      hoverPage: "",
+      pages: [
+        {
+          name: "Home",
+          to: "/"
+        },
+        {
+          name: "Chapters",
+          to: "/",
+          children: chaptersChildren
+        },
+        {
+          name: "About this Site",
+          to: "/sandbox"
+        }
+      ]
+    };
+  },
+  methods: {
+    onHover(page) {
+      this.hoverPage = page.
+      console.log(page);
+    }
+  }
+};
 </script>
-
-<style scoped>
-.site-nav-root {
-  display: flex;
-  justify-content: center;
-  background-color: black;
-}
-.flex {
-  width: 1100px;
-  color: white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 48px;
-}
-
-.site-title {
-  font-family: "VTC William";
-  font-size: 24px;
-}
-
-.nav-title {
-  font-family: "neue-haas-unica";
-  font-size: 20px;
-}
-
-</style>
