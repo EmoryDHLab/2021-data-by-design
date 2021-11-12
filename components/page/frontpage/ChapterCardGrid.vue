6d89c0a442dfff7957960a704e648ba5162eca58
Jianing Fu <annfu04@gmail.com>
2021-11-11 23:33:33 -0800

    homepage chapter cards grid

---
+++ components/page/frontpage/ChapterCardGrid.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="grid grid-cols-2 grid-rows-3">
+    <ChapterCard class="bg-green-300">
+      <template v-slot:title>Visualization as Argument</template>
+      <template v-slot:subtitle>William Playfair’s Time-Series Charts</template>
+      <template v-slot:caption>The Amalgamation of White and Black elements of the population in the United States by W.E.B Du Bois.
+        Atlanta University. Library of Congress.</template>
+    </ChapterCard>
+    <ChapterCard class="bg-yellow-200">
+      <template v-slot:title>This is a title</template>
+      <template v-slot:subtitle>This is a subtitle</template>
+      <template v-slot:caption>The Amalgamation of White and Black elements of the population in the United States by W.E.B Du Bois.
+        Atlanta University. Library of Congress.</template>
+    </ChapterCard>
+    <ChapterCard class="bg-royalblue">
+      <template v-slot:title>This is a title</template>
+      <template v-slot:subtitle>This is a subtitle</template>
+      <template v-slot:caption>The Amalgamation of White and Black elements of the population in the United States by W.E.B Du Bois.
+        Atlanta University. Library of Congress.</template>
+    </ChapterCard>
+    <ChapterCard class="bg-purple-300">
+      <template v-slot:title>This is a title</template>
+      <template v-slot:subtitle>This is a subtitle</template>
+      <template v-slot:caption>The Amalgamation of White and Black elements of the population in the United States by W.E.B Du Bois.
+        Atlanta University. Library of Congress.</template>
+    </ChapterCard>
+    <ChapterCard class="bg-red-300 col-span-2">
+      <template v-slot:title>This is a title</template>
+      <template v-slot:subtitle>This is a subtitle</template>
+      <template v-slot:caption>The Amalgamation of White and Black elements of the population in the United States by W.E.B Du Bois.
+        Atlanta University. Library of Congress.</template>
+    </ChapterCard>
+  </div>
+</template>
+
+<script>
+import ChapterCard from "./ChapterCard";
+export default {
+  components: {ChapterCard},
+  name: "ChapterCardGrid"
+}
+</script>
+
+<style scoped>
+
+</style>
