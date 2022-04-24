17759e4d8348270d0a773fa192a0fd14c48b5968
Dan Jutan <danjutan@gmail.com>
2022-04-23 23:12:38 -0400

    clean up setup

--- astro/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "astro/config";
-import nodejs from "@astrojs/node";
-
-// https://astro.build/config
-export default defineConfig({
-  adapter: nodejs()
-});
---
+++ src/fetch/google-doc.ts
@@ -0,0 +1,17 @@
+import { getDoc, parseDoc } from "google-docs-parser";
+
+const private_key = import.meta.env.PRIVATE_KEY_64
+  ? Buffer.from(import.meta.env.PRIVATE_KEY_64, "base64")
+  : import.meta.env.PRIVATE_KEY;
+
+const config = {
+  client_email: import.meta.env.CLIENT_EMAIL,
+  private_key,
+};
+
+console.log(config);
+
+export default async function get(doc) {
+  const docDownload = parseDoc(await getDoc(config, doc));
+  return docDownload;
+}
