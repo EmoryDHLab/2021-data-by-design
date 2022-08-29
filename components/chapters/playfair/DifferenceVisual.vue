a68bf515f4ee0a01f28125011e351a37608fa072
Nicholas Yang <nick@nicholasyang.com>
2022-08-29 13:42:51 -0400

    Removed another static data

--- components/chapters/playfair/DifferenceVisual.vue
+++ components/chapters/playfair/DifferenceVisual.vue
@@ -1,20 +1,17 @@
 <template>
-  <StaticData :dataset="['playfair_nums_def']" v-slot="data">
-    <svg
-      v-if="data.playfair_nums_def"
-      class="w-full h-full flex ml-6"
-      viewBox="0 0 100 50"
-    >
-      <rect width="100%" height="100%" fill="#F3ECCB" />
-      <Recreation :playfairData="data.playfair_nums_def"></Recreation>
-    </svg>
-  </StaticData>
+  <svg v-if="playfairData" class="w-full h-full flex ml-6" viewBox="0 0 100 50">
+    <rect width="100%" height="100%" fill="#F3ECCB" />
+    <Recreation :playfairData="playfairData"></Recreation>
+  </svg>
 </template>
 <script>
 import Recreation from "~/components/chapters/playfair/recreationElements/Recreation";
-import StaticData from "~/components/data-access/StaticData";
+import playfairData from "~/api/static/data/playfair_nums_def.json";
 
 export default {
-  components: { Recreation, StaticData },
+  components: { Recreation },
+  data() {
+    return { playfairData };
+  },
 };
 </script>
