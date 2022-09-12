b1afa92fe486545c8a9a4858be4832d922af1aaf
Nicholas Yang <nick@nicholasyang.com>
2022-09-12 13:27:32 -0400

    Modified Google Docs API code so that it saves the files in JSON

---
+++ pages/chapters/brooks.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ChapterLayout
+      v-if="docsData"
+      :docData="docsData"
+      :config="config"
+    ></ChapterLayout>
+  </div>
+</template>
+
+<script>
+import ChapterLayout from "~/components/page/chapter/ChapterLayout";
+import chaptersConfig from "~/chaptersConfig";
+import docsData from "~/docs/brooks.json";
+
+export default {
+  data() {
+    const config = chaptersConfig.brooks;
+
+    return {
+      docsData,
+      config,
+    };
+  },
+  computed: {},
+  mounted() {},
+  components: { ChapterLayout },
+};
+</script>
+
+<style></style>
---
+++ pages/chapters/dubois.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ChapterLayout
+      v-if="docsData"
+      :docData="docsData"
+      :config="config"
+    ></ChapterLayout>
+  </div>
+</template>
+
+<script>
+import ChapterLayout from "~/components/page/chapter/ChapterLayout";
+import chaptersConfig from "~/chaptersConfig";
+import docsData from "~/docs/dubois.json";
+
+export default {
+  data() {
+    const config = chaptersConfig.dubois;
+
+    return {
+      docsData,
+      config,
+    };
+  },
+  computed: {},
+  mounted() {},
+  components: { ChapterLayout },
+};
+</script>
+
+<style></style>
