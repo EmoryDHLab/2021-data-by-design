93d1a8f332fcd599bed52021786abb065b9753be
Nicholas Yang <nick@nicholasyang.com>
2022-09-12 20:44:01 -0400

    Okay fixed now

---
+++ pages/chapters/willard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ChapterLayout
+      v-if="docsData"
+      :docData="docsData"
+      :config="config"
+    ></ChapterLayout>
+  </div>
+</template>
+
+<script>
+import ChapterLayout from "~/components/page/chapter/ChapterLayout";
+import chaptersConfig from "~/chaptersConfig";
+import docsData from "~/docs/willard.json";
+
+export default {
+  data() {
+    const config = chaptersConfig.willard;
+
+    return {
+      docsData,
+      config,
+    };
+  },
+  computed: {},
+  mounted() {},
+  components: { ChapterLayout },
+};
+</script>
+
+<style></style>
